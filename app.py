7887736140878e3c3e66c514cb96820d2b8800e6
tnuma <tnuma.tech@gmail.com>
2025-05-14 14:12:10 +0900

    Add CORS support

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, Response
 import requests
 from bs4 import BeautifulSoup
+from flask_cors import CORS
+
 
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def home():
